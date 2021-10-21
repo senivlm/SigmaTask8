0f52fcfa902c73eb035e22549aacd38e432dfd5c
masiuko@ukr.net <masiuko@ukr.net>
2021-10-21 20:01:01 +0300

    Add project files.

---
+++ Task1/SortClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigmaTask8.Task1
+{
+    delegate int SortDelegate(Product p1, Product p2);
+    static class SortClass
+    {
+        public static void Sort(Product[] obj_arr, SortDelegate deleg )
+        {
+            
+        } 
+    }
+}
